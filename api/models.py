15c6c08832a2f3837b317b308c7be48d6c7bf616
Mohammed Benhelli <mohammed.benhelli@visian.fr>
2021-04-16 13:28:20 +0200

    push

---
+++ api/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Account(models.Model):
+    STUDENT = 'Student'
+    INSTRUCTOR = 'Instructor'
+    SECRETARY = 'Secretary'
+    ADMIN = 'Admin'
+    ROLE = (
+        (STUDENT, 'Student'),
+        (INSTRUCTOR, 'Instructor'),
+        (SECRETARY, 'Secretary'),
+        (ADMIN, 'Admin')
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
+    role = models.CharField(max_length=50, choices=ROLE, default=STUDENT)
+    hours = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.user.first_name + ' ' + self.user.last_name
+
+class Appointment(models.Model):
+    student = models.OneToOneField(Account, on_delete=models.CASCADE, related_name='student')
+    hour = models.DateTimeField()
+    lieux = models.CharField(max_length=50)
+    date = models.DateField()
+    instructor = models.OneToOneField(Account, on_delete=models.CASCADE, related_name='instructor')
---
+++ api/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from api.models import Account, Appointment
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = 'username', 'email', 'first_name', 'last_name'
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Account
+        fields = '__all__'
+
+class AppointmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appointment
+        fields = '__all__'
